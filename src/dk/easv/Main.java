b37ecd34d278d4fc154879f183505f6f713a8203
andersthomsen92 <doomknight92@gmail.com>
2024-02-06 22:47:49 +0100

    Styled the login window with a stylesheet.css

--- src/dk/easv/Main.java
+++ src/dk/easv/Main.java
@@ -14,6 +14,16 @@ public class Main extends Application {
         primaryStage.setTitle("Movie Recommendation System 0.01 Beta");
         // primaryStage.setFullScreen(true);
         primaryStage.setScene(new Scene(root));
+
+        /*
+        Makes the login window unscalable.
+        primaryStage.setMinWidth(914);
+        primaryStage.setMaxWidth(914);
+        primaryStage.setMinHeight(512);
+        primaryStage.setMaxHeight(512);
+        */
+
+
         primaryStage.show();
     }
 
